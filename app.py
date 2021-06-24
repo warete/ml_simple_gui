55f71ccf5e9efe0cfdd5b30c88ada3855caef6ca
Egor Borisovsky <egor@intervolga.ru>
2021-06-24 19:21:55 +0300

    Загрузка файла

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_url_path='', static_folder='./frontend/dist', template_folder='./frontend/dist')
 app.debug = True
+app.config['UPLOAD_FOLDER'] = os.path.join('upload')
 CORS(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/upload_data/', methods=['POST'])
+def upload_data():
+    file = request.files['file']
+    if file:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        return jsonify({
+            'status': 'success',
+            'result': {
+                'file_path': file_path
+            }
+        })
+    else:
+        return jsonify({
+            'status': 'error',
+            'result': {
+                'message': 'Неподходящий тип файла'
+            }
+        })
 
 
 if __name__ == '__main__':
