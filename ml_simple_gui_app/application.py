eaed91fc89bb83f642cb08bf960310492bd1cbee
Egor Borisovsky <mr-warete@mail.ru>
2021-06-26 14:43:41 +0300

    Вызов методов модели

--- ml_simple_gui_app/application.py
+++ ml_simple_gui_app/application.py
@@ -1,16 +1,22 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from .utils import check_model, calculate_sensitivity, calculate_specificity, get_data_from_csv
 import os
+import json
 
 
 class Application:
     model = None
     upload_path = '/upload'
     server_port = 5000
+    csv_delimiter = ','
 
-    def __init__(self, model, upload_path, server_port=5000):
+    def __init__(self, model, upload_path, csv_delimiter, server_port=5000):
         self.model = model
         self.upload_path = upload_path
+        self.csv_delimiter = csv_delimiter
         self.server_port = server_port
 
     def set_model(self, model):
@@ -23,6 +29,10 @@ class Application:
 
     def set_server_port(self, port):
         self.server_port = port
+        return self
+
+    def set_csv_delimiter(self, csv_delimiter):
+        self.csv_delimiter = csv_delimiter
         return self
 
     def run(self):
@@ -68,11 +78,41 @@ class Application:
 
         @app.route('/fit_predict/', methods=['POST'])
         def fit_predict():
+            # {'file': {'file_path': 'test.csv'}, 'testPercent': 25, 'learningRate': 0.01, 'learningEpochs': 1000}
+            req_params = json.loads(request.get_data())
+            if len(req_params['file']['file_path']) == 0 or not os.path.isfile(
+                    os.path.join(self.upload_path, req_params['file']['file_path'])):
+                return jsonify({
+                    'status': 'error',
+                    'result': {
+                        'message': 'Файл не существует'
+                    }
+                })
+            if not check_model(self.model):
+                return jsonify({
+                    'status': 'error',
+                    'result': {
+                        'message': 'Ошибка при использовании модели. Проверьте наличие необходимых методов'
+                    }
+                })
+            data_from_file = get_data_from_csv(
+                os.path.join(self.upload_path, req_params['file']['file_path']), self.csv_delimiter)
+            x = data_from_file.iloc[:, :-1]
+            y = data_from_file.iloc[:, -1:]
+
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=int(req_params['testPercent']) / 100)
+
+            self.model.fit(x_train, y_train)
+            y_pred = self.model.predict(x_test)
+            print(y_test, y_pred)
+            accuracy = accuracy_score(y_test, y_pred)
+            sensitivity = calculate_sensitivity(y_test, y_pred) * 100
+            specificity = calculate_specificity(y_test, y_pred) * 100
             return jsonify({
                 'status': 'success',
                 'result': {
-                    'accuracy': 0,
-                    'sensitivity': 0,
-                    'specificity': 0,
+                    'accuracy': accuracy,
+                    'sensitivity': sensitivity,
+                    'specificity': specificity,
                 }
             })
--- ml_simple_gui_app/utils.py
+++ ml_simple_gui_app/utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from io import StringIO
+
+
 def calculate_sensitivity(y_test, y_pred):
     sick_test_cnt = len(y_test[y_test == 1])
     sick_pred_cnt = len(y_pred[y_pred == 1])
@@ -8,3 +12,26 @@ def calculate_specificity(y_test, y_pred):
     healthy_test_cnt = len(y_test[y_test == 0])
     healthy_pred_cnt = len(y_pred[y_pred == 0])
     return healthy_pred_cnt / (healthy_pred_cnt + abs(healthy_test_cnt - healthy_pred_cnt))
+
+
+def check_model(model):
+    fit_method = getattr(model, 'fit', None)
+    if not callable(fit_method):
+        return False
+
+    predict_method = getattr(model, 'predict', None)
+    if not callable(predict_method):
+        return False
+
+    return True
+
+
+def get_data_from_csv(file_path, delimiter):
+    try:
+        with open(file_path, "r") as f:
+            data = pd.read_csv(StringIO(f.read()), delimiter=delimiter)
+            f.close()
+            return data
+    except Exception as error:
+        print(error)
+        return pd.DataFrame
