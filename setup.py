064fe39f6ddb3c8127576a81eb5f2e56aa0a306d
Egor Borisovsky <mr-warete@mail.ru>
2021-06-27 14:33:11 +0300

    Фикс формата long_description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='1.0.3',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
     install_requires=[item.replace('\n', '') for item in open(join(dirname(__file__), 'requirements.txt')).readlines()],
     include_package_data=True,
 )
