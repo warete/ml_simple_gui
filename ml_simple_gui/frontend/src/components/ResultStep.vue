454574e3f5223c8541b3a119f9cd941cd88314f5
Egor Borisovsky <egor@intervolga.ru>
2021-10-11 20:52:57 +0300

    Поддержка сложных структур в метриках. Изменил компонент для вывода метрик на таблицу

--- ml_simple_gui/frontend/src/components/ResultStep.vue
+++ ml_simple_gui/frontend/src/components/ResultStep.vue
@@ -1,57 +1,99 @@
 <template>
-    <v-row>
-        <v-col cols="6">
-            <v-skeleton-loader
-                    v-if="resultLoading"
-                    type="list-item-three-line, list-item-three-line, image, actions"
-            ></v-skeleton-loader>
-            <template
-                    v-else
+  <v-row>
+    <v-col cols="12">
+      <div v-if="resultLoading">
+        <v-alert
+          border="left"
+          color="indigo"
+          dark
+        >
+          Идет загрузка данных
+        </v-alert>
+        <v-skeleton-loader
+            type="list-item-three-line, list-item-three-line, image, actions"
+        ></v-skeleton-loader>
+      </div>
+      <template
+          v-else
+      >
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+            <tr>
+              <th class="text-left">
+                Параметр
+              </th>
+              <th class="text-left">
+                Результат
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="metric in formattedMetrics"
+                :key="metric.code"
             >
-                <v-list class="transparent">
-                    <v-list-item v-for="metric in metrics" :key="metric.code">
-                        <v-list-item-title>{{metric.name}}</v-list-item-title>
-                        <v-list-item-subtitle class="text-right">
-                            {{ metric.result }}
-                        </v-list-item-subtitle>
-                    </v-list-item>
-                </v-list>
-            </template>
-        </v-col>
-    </v-row>
+              <td>{{ metric.name }}</td>
+              <td>
+                <div v-html="metric.result"/>
+              </td>
+            </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </template>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-    import {mapActions, mapGetters} from "vuex";
+import {mapActions, mapGetters} from "vuex";
+import {map, isArray, isString, isObject} from 'lodash';
 
-    export default {
-        name: "ResultStep",
-        props: ['isOpened'],
-        methods: {
-            ...mapActions({
-                'fetchResults': 'main/fetchResults',
-            }),
-        },
-        computed: {
-            ...mapGetters({
-                'trainTestDataFile': 'main/trainTestDataFile',
-                'learningEpochs': 'main/learningEpochs',
-                'modelParams': 'main/modelParams',
-                'metrics': 'main/result',
-                'resultLoading': 'main/resultLoading',
-            })
-        },
-        watch: {
-            isOpened: function (val) {
-                if (val) {
-                    this.fetchResults({
-                        file: this.trainTestDataFile,
-                        modelParams: this.modelParams,
-                    });
-                }
-            },
-        },
+export default {
+  name: "ResultStep",
+  props: ['isOpened'],
+  methods: {
+    ...mapActions({
+      'fetchResults': 'main/fetchResults',
+    }),
+  },
+  computed: {
+    ...mapGetters({
+      'trainTestDataFile': 'main/trainTestDataFile',
+      'learningEpochs': 'main/learningEpochs',
+      'modelParams': 'main/modelParams',
+      'metrics': 'main/result',
+      'resultLoading': 'main/resultLoading',
+    }),
+    formattedMetrics() {
+      return map(this.metrics, metric => {
+        if (isString(metric.result)) {
+          return metric;
+        }
+        if (isArray(metric.result)) {
+          return {...metric, result: metric.result.join(', ')};
+        }
+        if (isObject(metric.result)) {
+          return {
+            ...metric,
+            result: [...map(metric.result, (metricItem, metricItemKey) => `${metricItemKey}: ${metricItem}`)].join('<br>')
+          };
+        }
+      })
     }
+  },
+  watch: {
+    isOpened: function (val) {
+      if (val) {
+        this.fetchResults({
+          file: this.trainTestDataFile,
+          modelParams: this.modelParams,
+        });
+      }
+    },
+  },
+}
 </script>
 
 <style scoped>
